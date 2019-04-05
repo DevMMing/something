2f8093467a4c732c34a921ad46b356c960a198ed
Matthew Ming <34111403+DevMMing@users.noreply.github.com>
2019-04-04 20:41:29 -0400

    finally

---
+++ script.py
@@ -0,0 +1,37 @@
+f=open('NYPD_Motor_Vehicle_Collisions.csv',"r")
+num_lines = sum(1 for line in open('NYPD_Motor_Vehicle_Collisions.csv'))
+print(num_lines)
+a=0
+b=0
+c=0
+d=0
+e=0
+g=0
+for i in range(num_lines):
+    if i==0:
+        f.readline()
+    else:
+        string=f.readline().split(",")[2]
+        if string=="BRONX":
+            a+=1
+        if string=="MANHATTAN":
+            b+=1
+        if string=="BROOKLYN":
+            c+=1
+        if string=="QUEENS":
+            d+=1
+        if string=="STATEN ISLAND":
+            e+=1
+        if string == "":
+            g+=1
+f.close()
+print("done")
+f=open("save.csv","w")
+f.write("Boroughs,Accidents\n")
+f.write("Bronx,"+str(a)+"\n")
+f.write("Manhattan,"+str(b)+"\n")
+f.write("Brooklyn,"+str(c)+"\n")
+f.write("Queens,"+str(d)+"\n")
+f.write("Staten Island,"+str(e)+"\n")
+f.write("Unknown,"+str(g))
+f.close()
